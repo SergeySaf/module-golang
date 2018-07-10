c7e827241b19dc65d810163c9d0d5b459032a7c6
dmytro <dmytroyesin@gmail.com>
2018-07-10 15:14:20 +0300

    ex1

---
+++ ex1/downcase.go
@@ -0,0 +1,38 @@
+package downcase
+
+func ByteToString(c []byte) string {
+	n := -1
+	for i, b := range c {
+		if b == 0 {
+			break
+		}
+		n = i
+	}
+	return string(c[:n+1])
+}
+
+func Downcase(s string) (string, error) {
+	up := "QWERTYUIOPASDFGHJKLZXCVBNM"
+	lenn := len(s)
+	var barr [100]byte
+	for i := 0; i < lenn; i++ {
+		flag := 0
+		for k := 0; k < 26; k++ {
+			if s[i] == up[k] {
+				barr[i] = s[i] + 32
+				flag = 1
+			}
+			if flag == 0 {
+				barr[i] = s[i]
+			}
+		}
+	}
+	res := ByteToString(barr[:])
+	return res, nil
+}
+
+/*func main() {
+	st := "CryptoCurrency Exange PEATIO"
+	str := downcase(st)
+	fmt.Println(str)
+}*/
