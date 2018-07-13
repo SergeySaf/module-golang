8b03685475fc1784a2191ccddcd7dbd6ba51a9ce
dmytro <dmytroyesin@gmail.com>
2018-07-13 11:26:30 +0300

    ex04_2

--- ex04/bracket.go
+++ ex04/bracket.go
@@ -1,10 +1,39 @@
 package brackets
 
+type Stack struct {
+	i  int
+	st []byte
+}
+
+func New() *Stack {
+	return &Stack{}
+}
+
+func (c *Stack) Push(numb byte) {
+	c.i++
+	c.st = append(c.st, numb)
+}
+
+func (c *Stack) Pop() byte {
+	c.i--
+	res := c.st[c.i]
+	c.st = c.st[:c.i]
+	return res
+}
+
+func (c *Stack) Status() int {
+	return c.i
+}
+
+func (c *Stack) Read() byte {
+	res := c.st[(c.i - 1)]
+	return res
+}
+
 func Bracket(str string) (bool, error) {
+	var c *Stack = New()
 	openn := "[{("
 	closse := "]})"
-	var st [100]byte
-	n := 0
 	lens := len(str)
 	if lens == 0 {
 		return true, nil
@@ -12,21 +41,21 @@ func Bracket(str string) (bool, error) {
 	for i := 0; i < lens; i++ {
 		for k := 0; k < 3; k++ {
 			if str[i] == openn[k] {
-				st[n] = str[i]
-				n++
+				c.Push(str[i])
+				// n++
 			} else if str[i] == closse[k] {
-				if (n - 1) < 0 {
+				if c.Status() < 1 {
 					return false, nil
 				}
-				if st[n-1] == openn[k] {
-					n--
+				if c.Read() == openn[k] {
+					c.Pop()
 				} else {
 					return false, nil
 				}
 			}
 		}
 	}
-	if n == 0 {
+	if c.Status() == 0 {
 		return true, nil
 	}
 	return false, nil
