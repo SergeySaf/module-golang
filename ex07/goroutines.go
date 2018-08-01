d658b036bbb3d2e82f5d02199015887af9d43532
dmytro <dmytroyesin@gmail.com>
2018-08-01 11:36:15 +0300

    ex07New

--- ex07/goroutines.go
+++ ex07/goroutines.go
@@ -1,29 +1,50 @@
 package goroutines
 
-func Change_str(input, output chan string, close_sig chan bool) {
-	var buff []byte
-	str := <-input
-	buff = append(buff, '(')
-	for i := 0; i < len(str); i++ {
-		buff = append(buff, str[i])
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+var wait_buf sync.WaitGroup
+var stop sync.Mutex
+
+func Workers(tasks chan string, number int) {
+	defer wait_buf.Done()
+	flag := 0
+	stop.Unlock()
+
+	for i := range tasks {
+		if flag == 0 {
+			fmt.Printf("worker:%d spawning\n", number)
+		}
+		flag++
+		timer, _ := time.ParseDuration(i + "s")
+		fmt.Printf("worker:%d sleep:%s\n", number, i)
+		time.Sleep(timer)
 	}
-	buff = append(buff, ')')
-	str2 := string(buff[:])
-	output <- str2
-	close_sig <- true
+
+	if flag > 0 {
+		fmt.Printf("worker:%d stopping\n", number)
+	}
 }
 
-func Chan_close(close_sig chan bool, output chan string) {
-	s := <-close_sig
-	if s == true {
-		close(output)
-	}
-}
+func Run(num int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	tasks := make(chan string, 64)
 
-func Process(input chan string) chan string {
-	close_sig := make(chan bool)
-	output := make(chan string)
-	go Change_str(input, output, close_sig)
-	go Chan_close(close_sig, output)
-	return output
+	for scanner.Scan() {
+		input := string(scanner.Bytes())
+		tasks <- input
+	}
+
+	for i := 1; i <= num; i++ {
+		wait_buf.Add(1)
+		stop.Lock()
+		go Workers(tasks, i)
+	}
+	close(tasks)
+	wait_buf.Wait()
 }
